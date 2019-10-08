189425b2192440eeff4792c3245818d831945f0f
20181102931 <18647197975@163.com>
2019-10-08 16:05:23 +0800

    Initial Commit

---
+++ 运动会分数/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  运动会分数
+//
+//  Created by s20181102931 on 2019/10/8.
+//  Copyright © 2019 s20181102931. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
